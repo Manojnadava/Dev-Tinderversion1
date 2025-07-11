d31c1be9bc777b7b379e66f5ee780158e1186aa2
Manoj Jaya Shetty <mshetty@oneinc.com>
2025-07-11 20:21:41 +0530

    App_Router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const express=require('express');
 
 const app= express(); // creating an instance of an express through calling function
 
+app.get("/user",(req,res)=>{
+    res.send({firstName: 'Mw]eo', LastName:'hij'});
+})
+
 app.use("/test",(req,res)=>{
     res.send("Hello from server for /test")
 })   // req and res hanlder for url /test
@@ -12,6 +16,8 @@ app.use((req,res)=>{
     res.send("Hello from server")
 })   // req and res hanlder
 
+
+//app.use will match all http method routes
 app.use("/test",(req,res)=>{
     res.send("Hello from server for test")
 })   // req and res hanlder for url /test
